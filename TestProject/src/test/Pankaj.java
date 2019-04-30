6ef776c8a1b4a3b7a4b82d0cda61980b738d96c9
pankajyoctal <pankajy@octalframes.com>
2019-04-30 12:32:43 +0530

    Pankaj.java class added

---
+++ TestProject/src/test/Pankaj.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Pankaj {
+public static String msg1() {
+	return "I'm fine";
+	
+}
+
+}
--- TestProject/src/test/Test.java
+++ TestProject/src/test/Test.java
@@ -5,9 +5,10 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("Test is successful");
-
+Pankaj pk=new Pankaj();
   Deepak dp=new Deepak();
   System.out.println(dp.msg());
+  System.out.println(pk.msg1());
 	}
 	
 	
